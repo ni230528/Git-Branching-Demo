f6324035ace0d234ca5b5a0633d598f918ee068f
ni230528 <ng.chi.nghi@gmail.com>
2026-06-28 22:03:47 +0700

    feat: handle divide by zero with sentinel -1

--- math.cpp
+++ math.cpp
@@ -11,6 +11,11 @@ int multiply(int a , int b){
     else return a*b;
 }
 
+int divide(int a, int b){
+    if(b==0) return -1;
+    else return a/b;
+}
+
 int main(){
     cout << "2+3=" << add(2,3) << endl;
     cout << "2*3=" << multiply(2,3) << endl;
